c1ba65b14a325f0663840732dade6c585230ada6
aaaaa <gopmaster@hotmail.com>
2025-06-18 21:32:18 +0800

    加入email

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,16 @@ export default function Home() {
       <h1>aaaaa</h1>
       <h3>在遊戲產業二十年左右，經歷遊戲企畫、獨立開發等工作，目前主要自由接案。</h3>
       <h3>接案項目：小遊戲開發、Spine動作設計、Live2D動作設計</h3>
+      <p>
+        若您對我的作品有興趣，歡迎來信聯絡：{' '}
+        <a href="mailto:tppr2046@gmail.com" style={{ color: '#007bff' }}>
+          tppr2046@gmail.com
+        </a>
+      </p>
+
     </main>
+
+    
   );
 
   

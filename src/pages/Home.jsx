96c1f9d17c22549b6dadc2490e13600d6f557ab6
aaaaa <gopmaster@hotmail.com>
2025-06-18 21:55:59 +0800

    修正文字

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <h1>aaaaa</h1>
-      <h3>在遊戲產業二十年左右，經歷遊戲企畫、獨立開發等工作，目前主要自由接案。</h3>
-      <h3>接案項目：小遊戲開發、Spine動作設計、Live2D動作設計</h3>
+      <h1>aaaaa工作室</h1>
+      <h3>在遊戲產業多年，經歷遊戲企畫、獨立遊戲開發、視覺設計等工作</h3>
+      <h3>接案項目：小遊戲開發、Spine動作設計、Live2D動作設計、視覺設計</h3>
       <p>
         若您對我的作品有興趣，歡迎來信聯絡：{' '}
         <a href="mailto:tppr2046@gmail.com" style={{ color: '#007bff' }}>

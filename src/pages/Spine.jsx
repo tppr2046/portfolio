07b3eec18fbc1233350c803c7477738d7ad4f2bc
aaaaa <gopmaster@hotmail.com>
2025-11-04 14:55:38 +0800

    update spine case

--- src/pages/Spine.jsx
+++ src/pages/Spine.jsx
@@ -2,17 +2,18 @@ import './Spine.css'
 
 export default function Spine() {
   const videos = [
-  'https://www.youtube.com/embed/k7v-WNCklg4?si=B1nRPeC2GghLEbDN',
-  'https://www.youtube.com/embed/IsF_QqMdPWQ?si=YwvYanrsR0fdlMex',
-  'https://www.youtube.com/embed/CJirXArQlow?si=Gr3ARCTr0ZICM-xk',
-  'https://www.youtube.com/embed/WE4phYIDoV4?si=VSPXaJ3p7mr7znXm',
-  'https://www.youtube.com/embed/81U4UiX2p24?si=f-TQA8zebCVcqcof',
-  'https://www.youtube.com/embed/W9eh_xAhsmA?si=JBhpfVC4D3U83_jy',
-  'https://www.youtube.com/embed/INBzHr1HHPM?si=FJdGMwxb01jxEoP4',
-  'https://www.youtube.com/embed/wGI0eDmvU9A?si=UZk96GZryrJ9EZYc',
-  'https://www.youtube.com/embed/UuSFmhB7TL0?si=aE-TjSH6Cdd-gld1',
-  'https://www.youtube.com/embed/OTuer4XaD2Y?si=fHjWS_7x1q6CfXUW',
-  'https://www.youtube.com/embed/dUsFTwnFykY?si=bnnGp5UvPn_FwUzt',
+  'https://www.youtube.com/embed/k7v-WNCklg4',
+  'https://www.youtube.com/embed/IsF_QqMdPWQ',
+  'https://www.youtube.com/embed/CJirXArQlow',
+  'https://www.youtube.com/embed/WE4phYIDoV4',
+  'https://www.youtube.com/embed/81U4UiX2p24',
+  'https://www.youtube.com/embed/W9eh_xAhsmA',
+  'https://www.youtube.com/embed/INBzHr1HHPM4',
+  'https://www.youtube.com/embed/wGI0eDmvU9A',
+  'https://www.youtube.com/embed/UuSFmhB7TL0',
+  'https://www.youtube.com/embed/OTuer4XaD2Y',
+  'https://www.youtube.com/embed/dUsFTwnFykY',
+  'https://www.youtube.com/shorts/PgW9UXpRszs',
 
 ]
   
